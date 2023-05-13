13354ebb76c4c35940497d3d9633cdc9f162d66f
jabamoha <jabamoha@netanya.ac.il>
2023-05-13 21:08:25 +0300

    fix bug in the root path

--- server/app/app.py
+++ server/app/app.py
@@ -4,6 +4,9 @@ from api.router import object_detection
 from api.router import users
 app = Flask(__name__)
 
+app.route('/', methods=['GET'])
+def root():
+    return "Welcome to the AR Snipper 3D API!"
 # register blueprint endpoints
 app.register_blueprint(object_detection.object_detect, url_prefix='/object_detect')
 app.register_blueprint(users.users)
